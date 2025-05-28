38ee37f769ca67efc4fb7a6426412618847c1e0b
Tr4cck <trackonyou@outlook.com>
2025-05-28 11:01:51 +0800

    :hammer: upgrade all configs

---
+++ zsh/env.zsh
@@ -0,0 +1,27 @@
+export ANDROID_HOME=$HOME/Android/Sdk
+export JAVA_HOME="/usr/lib/jvm/default"
+export GOPATH=$HOME/go
+export MANPATH="/usr/local/man:$MANPATH"
+
+export PATH=$PATH:$HOME/.local/bin
+export PATH=$PATH:$HOME/.cargo/bin
+export PATH=$PATH:$ANDROID_HOME/platform-tools
+
+export YSYX_HOME=$HOME/p/ysyx
+export NPC_HOME=$YSYX_HOME/workbench/npc
+export NVBOARD_HOME=$YSYX_HOME/workbench/nvboard
+export NEMU_HOME=$HOME/p/ics2024/nemu
+export AM_HOME=$HOME/p/ics2024/abstract-machine
+
+export WORKON_HOME=$HOME/Envs
+export VIRTUALENVWRAPPER_PYTHON=/bin/python
+
+export PYTHONDONTWRITEBYTECODE=1
+export RUST_BACKTRACE=full
+
+export DEBUGINFOD_URLS="https://debuginfod.archlinux.org"
+export DEBUGINFOD_PROGRESS=1
+
+export STARSHIP_CONFIG=$HOME/.config/starship/starship.toml
+
+export EDITOR=nvim
